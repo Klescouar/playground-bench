95eac3e043d798be101cf3144362f6e067113a00
Klescouar <lescouarneckevin@protonmail.com>
2023-06-29 16:09:00 -0400

    feat(loader): add loader before to display the screen (#8)

--- src/components/layout/canvas.tsx
+++ src/components/layout/canvas.tsx
@@ -3,9 +3,6 @@ import { Canvas } from '@react-three/fiber';
 import { A11yAnnouncer } from '@react-three/a11y';
 import { OrbitControls, Preload, Stats } from '@react-three/drei';
 import React from 'react';
-import localFont from 'next/font/local';
-
-const futura = localFont({ src: '../../styles/fonts/Gotham.otf' });
 
 const Controls = () => {
   const control = useRef(null);
@@ -13,7 +10,7 @@ const Controls = () => {
 };
 const CanvasWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className={`Canvas ${futura.className}`}>
+    <div className='Canvas'>
       <div className='Canvas__Frame'>
         <div className='Canvas__Frame__Header'>
           <h1 className='Canvas__Frame__Header__Name'>
