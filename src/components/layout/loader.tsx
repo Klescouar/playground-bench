7a9a087d594256be5ffa46013fb8d3cd1025368d
Klescouar <lescouarneckevin@protonmail.com>
2023-07-07 10:37:25 -0400

    improvement(load): set about screen as default screen (#19)

--- src/components/layout/loader.tsx
+++ src/components/layout/loader.tsx
@@ -1,21 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import classNames from 'classnames';
+import { LoadingContext } from '@/pages/_app';
 
 const Loader: React.FC = () => {
   const [hideText, setHideText] = useState(false);
+  const { setIsLoaded } = useContext(LoadingContext);
 
   useEffect(() => {
     const timeout = setTimeout(() => {
       setHideText(true);
-    }, 2000);
+      setTimeout(() => setIsLoaded?.(true), 1250);
+    }, 1600);
     return () => clearTimeout(timeout);
-  }, []);
+  }, [setIsLoaded]);
 
-  const textClassname = classNames('Loader__Text__Part', 'animate-fade', { disapear: hideText });
+  const textClassname = classNames('Loader__Text__Part', 'animate-fade', {
+    disapear: hideText,
+  });
 
   return (
     <div className={classNames('Loader', { hide: hideText })}>
-      <p className='Loader__Text'>
+      <p className="Loader__Text">
         <span className={textClassname}>Kevin Le Scouarnec</span>
         <span className={textClassname}>|</span>
         <span className={textClassname}>Curriculum Vitae</span>
